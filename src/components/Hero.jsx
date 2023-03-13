5c0f1ee2fde212c13494c576461cef2e8a60d023
unknown <bbabines@gmail.com>
2023-03-13 17:21:48 -0500

    Hero section completed.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,16 +18,28 @@ const Hero = () => {
 					<h1 className={`${styles.heroHeadText} text-white`}>
 						Hi, I'm <span className="text-[#915eff]">Brad</span>
 					</h1>
-					<p className={`${styles.heroSubText} mt-2 text-white-100`}>
-						I'm a React and Three.js developer
-						<span className="sm:block hidden">
-							that builds web sites, user interfaces, and web applications with
-							React and Three.js.
-						</span>
+					<p className={`${styles.heroSubText}  text-white-100`}>
+						I builds user interfaces, web applications, and websites with React
+						<span className="sm:hidden">and Three.js. </span>
 					</p>
 				</div>
 			</div>
 			<ComputersCanvas />
+			<div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+				<a href="#about">
+					<div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify center center items-start p-2">
+						<motion.div
+							animate={{ y: [0, 24, 0] }}
+							transition={{
+								duration: 1.5,
+								repeat: Infinity,
+								repeatType: "loop",
+							}}
+							className="w-3 h-3 rounded-full bg-secondary mb-1"
+						></motion.div>
+					</div>
+				</a>
+			</div>
 		</section>
 	);
 };
