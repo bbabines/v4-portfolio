0cdbb87439984bb2b4d39da8c827523c2afed91e
unknown <bbabines@gmail.com>
2023-03-14 14:53:25 -0400

    Tech section completed.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -41,7 +41,10 @@ const ExperienceCard = ({ experience }) => {
 			</div>
 			<ul className="mt-5 list-disc ml-5 space-y-2">
 				{experience.points.map((point, index) => (
-					<li className="text-white-100 text-[14px] pl-1 tracking-wider">
+					<li
+						key={index}
+						className="text-white-100 text-[14px] pl-1 tracking-wider"
+					>
 						{point}
 					</li>
 				))}
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../utils/motion";
+import { styles } from "../styles";
+
+import { BallCanvas } from "./canvas";
+import { technologies } from "../constants";
 
 const Tech = () => {
-  return (
-    <div>Tech</div>
-  )
-}
+	return (
+		<>
+			<motion.div variants={textVariant()}>
+				<p className={`${styles.sectionSubText}`}>
+					The technologies I work with
+				</p>
+				<h2 className={`${styles.sectionHeadText} mb-20`}>Tools.</h2>
+			</motion.div>
+			<div className="flex flex-row flex-wrap justify-center gap-10">
+				{technologies.map((technology) => (
+					<div className="w-28 h-28" key={technology.name}>
+						<BallCanvas icon={technology.icon} />
+					</div>
+				))}
+			</div>
+		</>
+	);
+};
 
-export default Tech
+export default SectionWrapper(Tech, "");
